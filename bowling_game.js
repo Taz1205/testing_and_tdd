5429b61d187d148fddc153fa6f2e734d046b330c
Fatima Zahra <fathima_z@yahoo.com>
2023-06-06 14:11:38 +0100

    Refactored my bowling game used arr instead of str

--- bowling_game.js
+++ bowling_game.js
@@ -1,60 +1,28 @@
-export const bowlingGame = (rolls) => {
+export function bowlingGame(rolls) {
+  let frame = 0;
   let score = 0;
-  let index = 0;
-  if (rolls === "XXXXXXXXXXXX") {
-    score = 300;
-  } 
-else {
-  const singleRoll = rolls.split("");
-  let frame = 1;
-  for (let i = 0; i < rolls.length && frame <= 10; i++) {
-    if (singleRoll[index] === "X") {
-    score += 10 + strikeScore(index, singleRoll);
-    console.log(" strike score: " + score);
-    index++;
-  }
-  if (singleRoll[index + 1] === "/") {
-    score += 10 + spareScore(index, singleRoll);
-    index += 2;
-  }
-   else  {
-      score += sumOfPinsInEachFrame(index, singleRoll);
-      index += 2; //each frame has 2 rolls
+  rolls.forEach(function (elem, index) {
+    if (frame < 10) {
+      if (rolls[index] === "X") {
+        score +=
+          pinScore(index, rolls) +
+          pinScore(index + 1, rolls) +
+          pinScore(index + 2, rolls);
+        frame += 1;
+      } else if (rolls[index] === "/") {
+        score += pinScore(index, rolls) + pinScore(index + 1, rolls);
+        frame += 0.5;
+      } else {
+        score += pinScore(index, rolls);
+        frame += 0.5;
+      }
     }
-    frame++;
-  }
-}
+  });
   return score;
-};
-
-function sumOfPinsInEachFrame(index, singleRoll) {
-  if (singleRoll[index] === "-") singleRoll[index] = 0;
-  if (singleRoll[index + 1] === "-") singleRoll[index + 1] = 0;
-  if (singleRoll[index + 1] === "/" || singleRoll[index] === "X" || singleRoll[index + 1] === "X") {
-    return 10;
-  }
-  return Number(singleRoll[index]) + Number(singleRoll[index + 1]);
 }
-function strikeScore(index, singleRoll) {
-  if (singleRoll[index + 1] === "-") singleRoll[index + 1] = 0;
-  if (singleRoll[index + 2] === "-") singleRoll[index + 2] = 0;
-  if (singleRoll[index + 1]  === "X" || singleRoll[index + 2]  === "X") {
-    return 10;
-  }
-  if (singleRoll[index + 1]  === "X" && singleRoll[index + 2]  === "X") {
-    return 20;
-  }
-  if ( singleRoll[index + 2]  === "/") {
-    return 10 - singleRoll[index + 1];
-  }
-  console.log( "strike score inside: " + (Number(singleRoll[index + 1]) + Number(singleRoll[index + 2])));
-  return Number(singleRoll[index + 1]) + Number(singleRoll[index + 2]);
-}
-
-function spareScore(index, singleRoll) {
-  if (singleRoll[index + 2] === "-") singleRoll[index + 2] = 0;
-  if (singleRoll[index + 2] === "/" || singleRoll[index + 2]  === "X") {
-    return 10;
-  }
-  return Number(singleRoll[index + 2]);
+function pinScore(index, rolls) {
+  if (rolls[index] === "-") return 0;
+  else if (rolls[index] === "X") return 10;
+  else if (rolls[index] === "/") return 10 - pinScore(index - 1, rolls);
+  else return rolls[index];
 }
--- bowling_game.test.js
+++ bowling_game.test.js
@@ -2,37 +2,39 @@ import {bowlingGame} from "./bowling_game.js";
   
   describe("returns scores for a bowling game based on the rolls input", () => {
     test("returns score 0 when all 20 rolls are misses(gutter game)", () => {
-      expect(bowlingGame("--------------------")).toEqual(0);
+      expect(bowlingGame(["-","-","-","-","-","-","-","-","-","-","-","-","-","-","-","-","-","-","-","-"])).toEqual(0);
     });
     test("returns score 300 when all 12 rolls are strikes", () => {
-      expect(bowlingGame("XXXXXXXXXXXX")).toEqual(300);
+      expect(bowlingGame(["X","X","X", "X","X", "X", "X", "X", "X", "X","X","X"])).toEqual(300);
     });
     test("returns score 150 when all rolls are spares", () => {
-      expect(bowlingGame("5/5/5/5/5/5/5/5/5/5/5")).toEqual(150);
+      expect(bowlingGame([5,"/",5,"/",5,"/",5,"/",5,"/",5,"/",5,"/",5,"/",5,"/",5,"/",5])).toEqual(150);
     });
     test("returns score 90 when 10 rolls are 9s and 10 misses", () => {
-      expect(bowlingGame("9-9-9-9-9-9-9-9-9-9-")).toEqual(90);
+      expect(bowlingGame([9,"-", 9,"-", 9,"-", 9,"-", 9,"-", 9,"-", 9,"-", 9,"-", 9,"-", 9,"-"])).toEqual(90);
     });
-    test("returns score 93 when all rolls 9 or less", () => {
-      expect(bowlingGame("12345678912345678912")).toEqual(93);
+    test("returns score 61 when no spares or no strikes", () => {
+      expect(bowlingGame([9,1, 3,5, "-","-", 4,2, 5,"-", 2,3, 6,3, 1,2, 7,1, 3,4])).toEqual(61);
     });
-    test("returns score 79  with a mix of pins knocked down and misses", () => {
-      expect(bowlingGame("77-9-8-886-6---81119")).toEqual(79);
+    test("returns score 20 when all pins are one", () => {
+      expect(bowlingGame([1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1])).toEqual(20);
     });
-    test("returns score 129 with a mix of spares and misses", () => {
-      expect(bowlingGame("5/9-5/8--85/9-185/5/5")).toEqual(129);
+    test("returns score 96 when there are mix of misses, strikes and spares", () => {
+      expect(bowlingGame([9,"/", 3,5, "-","-", "X", 5,"-", 2,3, 6,3, 1,2, 7,1, "X","X","X"])).toEqual(96);
     });
-     test("returns score 132 with a mix of spares and misses", () => {
-      expect(bowlingGame("7/9-9/81111/9-2/4/8/5")).toEqual(132);
+    test("returns score 80 when there are mix of strikes and spares", () => {
+      expect(bowlingGame([3,1, "X", 1,2, 1, "/", 3,"/", 1,1, 1,4, 5,3, 1,2, 6,"/",8])).toEqual(80);
     });
-    test("returns score 82 with a mix of spares and misses", () => {
-      expect(bowlingGame("587654321--81555115/5")).toEqual(82);
-    }); 
-    /*test("returns score 155 with a mix of strikes, spares and misses", () => {
-      expect(bowlingGame("X9-5/8-X-8/9-X8")).toEqual(155);
-    });*/ 
+    test("returns score 91 when there are mix of strikes and spares", () => {
+      expect(bowlingGame([2,1, 8,1, 3,5, "X", 2,6, 5,1, 4,5, 1,3, 5,1, "X",6,"/"])).toEqual(91);
+    });
+    test("returns score 94 with spares before the last turn", () => {
+      expect(bowlingGame([9,"/", 3,5, "-","/", 4,2, 5,"-", 2,"/", 6,3, 1,2, 7,"/", 3,4])).toEqual(94);
+    });
+    test("returns score 75 with strikes before the last turn", () => {
+      expect(bowlingGame([9,"-", "X", 1,1, 4,2, "X", "-","-", 6,3, 1,2, "X", 3,4])).toEqual(75);
+    });
   });
 
 
-
- 
+  
